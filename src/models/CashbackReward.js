9932d64c6098b5ebb2242a4ceff064afe711144b
kaylaknight385 <kaylaknight385@gmail.com>
2026-02-09 14:26:11 -0500

    adding cashbash schema noooow

---
+++ src/models/CashbackReward.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const cashbackRewardSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    platform: {
+        type: String, 
+        enum: ['Roblox', 'Steam', 'Twitch', 'Tiktok Shop', 'Playstation Store', 'Youtube', 'Spotify', 'Apple Music', 'Airbnb', 'Sephora', 'Ulta', 'Shein', 'Temu', 'Ticketmaster', 'AMC Theaters'],
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    transactionAmount: {
+        type: Number,
+        required: true,
+    }
+
+})
