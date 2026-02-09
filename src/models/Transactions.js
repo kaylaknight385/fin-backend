d9c8124547505e5c68369d0d360709ac30c19c8c
kaylaknight385 <kaylaknight385@gmail.com>
2026-02-09 12:01:24 -0500

    hold on, reviewed notes. Missed a couple more schemas to include

---
+++ src/models/Transactions.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    description: {
+        type: String, 
+        required: [true, 'Please provide a description'],
+        trim: true,
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Please provide an amount'],
+    },
+    type: {
+        type: String,
+        enum: ['income', 'expense'],
+        required: true,
+    }, 
+    catergory: {
+        type: String, 
+        enum: ['food', 'entertainment', 'shopping', 'bills', 'transport', 'gaming', 'other', 'salary', 'freelance'],
+        required: true,
+    },
+    date: {
+        type: Date, 
+        default: Date.now,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default mongoose.model('Transaction', transactionSchema);
