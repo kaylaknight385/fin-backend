b3451c2074d2b175027360f446061957e25c795a
kaylaknight385 <kaylaknight385@gmail.com>
2026-02-09 10:58:03 -0500

    database setup complete

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URQ
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connnected successfully');
+    } catch (error) {
+        console.log('MongoDB connection error:', error)
+        process.exit(1);
     }
-}
+};
+
+module.exports = connectDB;;
