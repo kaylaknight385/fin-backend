938262c5fb73add33bf4fe6b1bada088c4605328
kaylaknight385 <kaylaknight385@gmail.com>
2026-02-09 10:01:30 -0500

    finished user, email, and password schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose')
-const bcrypt = require
+const bcrypt = require('bcrypt')
+
+//user schemaaaaa omgggggggg
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true, 
+        unique: true, 
+        trim: true, 
+        minlength: [2, 'Name must be at least 2 characters'],
+        maxlength: [50, 'Name can not exceed 50 characters']
+    },
+    email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String, 
+    required: [true, 'Please provide password'],
+    minlength: [6, 'Password must be at lease 6 characters'],
+    select: false;
+  }
+})
