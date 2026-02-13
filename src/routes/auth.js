ccf3c2a7de4b76f14285965613100cbd3f87447b
kaylaknight385 <kaylaknight385@gmail.com>
2026-02-13 16:09:34 -0500

    creating users route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,2 +1,12 @@
 import express from 'express';
 import {signup, login, getMe, logout } from '../middleware/auth.js';
+import { authenticateToken } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/me', authenticateToken, getMe);
+router.post('/logout', authenticateToken, logout);
+
+export default router;
---
+++ src/routes/users.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+    getUserProfile,
+    updateUserProfile,
+    updateUserBalance,
+    deleteUser
+} from '../controllers/userController.js';
+import { authenticateToken } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/:id', authenticateToken, getUserProfile);
+router.put('/:id', authenticateToken, updateUserProfile);
+router.patch('/:id/balance', authenticateToken, updateUserProfile);
